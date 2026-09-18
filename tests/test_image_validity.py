d49120967e14c5730605b047ad9cda774d57bfad
Julien <julienknell@outlook.com>
2026-09-18 10:53:18 +1000

    feat: Added unit test for image validity specifically testing image size, blurry, dark and bright images and resizing the input image before validation so that the blur, dark and bright score remain consistent

--- tests/test_image_validity.py
+++ tests/test_image_validity.py
@@ -1,5 +1,9 @@
 from fastapi.testclient import TestClient
+import pytest
 from main import app
+from utils.image_verifier import is_validate
+from PIL import Image
+from io import BytesIO
 
 client = TestClient(app=app)
 
@@ -22,3 +26,41 @@ def test_invalid_image():
         )
 
     assert response.status_code == 400
+
+def test_valid_image_size():
+    with open("tests/assets/normal_size.png", "rb") as file:
+        image_bytes = Image.open(BytesIO(file.read())).convert("RGB")
+
+        result, compute_time = is_validate(image_bytes)
+
+    assert result is True
+    assert compute_time >= 0
+
+def test_invalid_image_size():
+    with open("tests/assets/small.png", "rb") as file:
+        image_bytes = Image.open(BytesIO(file.read())).convert("RGB")
+
+    with pytest.raises(ValueError):
+        is_validate(image_bytes)
+
+def test_blurry_image():
+    with open("tests/assets/small.png", "rb") as file:
+            image_bytes = Image.open(BytesIO(file.read())).convert("RGB")
+    
+    with pytest.raises(ValueError):
+        is_validate(image_bytes)
+
+def test_dark_image():
+    with open("tests/assets/dark.png", "rb") as file:
+        image_bytes = Image.open(BytesIO(file.read())).convert("RGB")
+
+    with pytest.raises(ValueError):
+        is_validate(image_bytes)
+
+def test_bright_image():
+    with open("tests/assets/bright2.png", "rb") as file:
+        image_bytes = Image.open(BytesIO(file.read())).convert("RGB")
+
+    with pytest.raises(ValueError):
+        is_validate(image_bytes)
+            
--- utils/image_verifier.py
+++ utils/image_verifier.py
@@ -1,6 +1,19 @@
 import cv2
 import numpy as np
 import time
+import json
+
+with open("config/config.json", "r") as config:
+    CONFIG = json.load(config)
+
+def resize_image(image):
+    target_width = CONFIG.get("target_image_resolution")
+    height, width = image.shape[:2]
+    
+    scale = target_width / width
+    new_height = int(height * scale)
+    
+    return cv2.resize(image, (target_width, new_height))
 
 def is_validate(raw_img):
 
@@ -13,12 +26,16 @@ def is_validate(raw_img):
 
     # 1. Image size verification
     height, width = img.shape[:2]
-    if height < 512 and width < 512:
+    if height < 224 and width < 224:
         raise ValueError("Image resolution too small. Please re-upload a high resolution image.")
 
-    # 2. Image blur verification
-    # Convert the image to grayscale so that changes in pixel intensity can be detected more easily. These changes represent edges in the image and can be used to determine whether the image is sharp or blurry.
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 2. Image visibility
+    # Convert the image to grayscale so that changes in pixel intensity can be detected more easily. These changes represent edges in the image and can be used to determine whether the image is dark or very bright.
+    resized_image = resize_image(img)
+    gray_img = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+
+    if _is_blurry(gray_image=gray_img):
+        raise ValueError("Image is too blurry.")
 
     # 3. Image brightness verification
     if gray_img.mean() < 40:
@@ -31,19 +48,24 @@ def is_validate(raw_img):
     
     return True, total_time
 
+def _is_blurry(gray_image, threshold=100):
+    blur_score = cv2.Laplacian(gray_image, cv2.CV_64F).var()
+    print(blur_score)
+    return blur_score < threshold
+
 # This function is only used for testing
-def __resize(image_path):
-    input_img = cv2.imread(image_path)
+# def __resize(image_path):
+#     input_img = cv2.imread(image_path)
 
-    output_img = cv2.resize(
-        input_img,
-        dsize=None,
-        fx=0.2,
-        fy=0.2,
-        interpolation=cv2.INTER_AREA
-    )
+#     output_img = cv2.resize(
+#         input_img,
+#         dsize=None,
+#         fx=0.2,
+#         fy=0.2,
+#         interpolation=cv2.INTER_AREA
+#     )
 
-    cv2.imwrite("testing/tmp_img/small.png", output_img)
+#     cv2.imwrite("testing/tmp_img/small.png", output_img)
 
 if __name__ == "__main__":
     is_validate("testing/tmp_img/normal.png")
